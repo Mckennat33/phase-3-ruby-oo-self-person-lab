13042136dbd90e2b2169353b15cee592ea6ae684
Thomas <thomasmckenna12gmail.com>
2022-08-31 19:05:39 -0500

    commit tree ffe06da4a348b22ac40f2c2319d346f8ab20a5f5

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,20 @@
 # your code goes here
+Class Person 
+    attr_reader :name
+    def name 
+        @name 
+    end
+    
+    def bank_account
+    
+    end
+
+    def happiness
+
+    end
+
+    def hygiene
+    
+    end
+
+end
